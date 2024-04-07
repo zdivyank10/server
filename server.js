0b09fe1a72f1c5ab57a4989c9abb176d21035d07
zdivyank <divyankzaveri10@gmail.com>
2024-04-07 20:29:02 +0530

    cors updated

--- server.js
+++ server.js
@@ -15,58 +15,40 @@ const blog = require('./models/blog-model')
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-
   destination: function (req, file, cb) {
     cb(null, './uploads')
   },
   filename: function (req, file, cb) {
-
     cb(null, file.originalname)
   }
 })
 const upload = multer({ storage: storage })
 
-
 const corsOptions = {
-  // origin:'https://inkgarden.info/',
-  origin: ["http://localhost:5173/","https://inkgarden.info/"],
-  methods: "GET,POST,DELETE,PUT,PATCH,HEAD",
-  credentials: true 
+  origin: ["http://localhost:5173", "https://inkgarden.info"],
+  methods: "GET, POST, DELETE, PUT, PATCH, HEAD",
+  credentials: true
 };
 
 app.use(cors(corsOptions));
-
-
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/form", contactRouter);
 app.use("/api/blog", blogRouter);
-// app.use('/api/blog/upload',express.static('uploads'))
-
-// let's define admin route
 app.use("/api/admin", adminRouter);
 app.use('/api/like', likeRoutes);
 app.use('/api/comment', commentRoutes);
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
 app.post('/api/blog/upload', upload.single('file'), function (req, res, next) {
-  // const imagePath = 'http://localhost:8000/' + req.file.path;
   res.json(req.file.filename);
 })
 app.put('/api/blog/:id/upload', upload.single('file'), function (req, res, next) {
-  // Assuming 'fileURL' is the URL where the uploaded file can be accessed
-  // const fileURL = `http://localhost:8000/uploads/${req.file.filename}`;
-
   res.json(req.file.filename);
-  // res.json({ fileURL });
 });
-
 
 app.get('/', async (req, res) => {
   try {
-   
     res.json('Hello Server');
   } catch (error) {
     console.error('Error fetching blog:', error);
@@ -82,20 +64,10 @@ app.get('/api/blog/:blogid', async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch blog' });
   }
 });
-// blog posting permission.....
-app.put('/api/admin/blog/:blogId/permission', blogRouter);
-
-
-
 app.use(errorMiddleware);
 
-connectDb().then(() => {
+app.listen(8000, () => {
+  console.log(`server running on port 8000`);
+});
 
-  app.listen(8000, () => {
-    console.log(`server running on port 8000`);
-  })
-  // app.listen(8000,()=>{
-  //     console.log(`server running on port 8000`);
-  // })
-})
 
