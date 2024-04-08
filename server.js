65b12d654db522cc8dac29ffe93c07955e275e32
zdivyank <divyankzaveri10@gmail.com>
2024-04-08 22:05:13 +0530

    wildcard cors

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const corsOptions = {
   credentials: true
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/form", contactRouter);
